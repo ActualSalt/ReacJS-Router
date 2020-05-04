303a0da188347ae92ce03605b8ef0ad56c020b91
MinLee <minlee0702@gmail.com>
2020-05-03 22:43:09 -0500

    Updates

---
+++ precache-manifest.4b38018aa6e5c4a2d7ab213bdc980724.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc07466e524b521dcab45fec1fabe9a",
+    "url": "/COVID-19-Summary/index.html"
+  },
+  {
+    "revision": "0b2d22c9d0915bb9d613",
+    "url": "/COVID-19-Summary/static/css/main.10e9d3dd.chunk.css"
+  },
+  {
+    "revision": "02cdc00f73af7af311d1",
+    "url": "/COVID-19-Summary/static/js/2.7394d84c.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/COVID-19-Summary/static/js/2.7394d84c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b2d22c9d0915bb9d613",
+    "url": "/COVID-19-Summary/static/js/main.268d40b7.chunk.js"
+  },
+  {
+    "revision": "a1b33100509ceb66d6ef",
+    "url": "/COVID-19-Summary/static/js/runtime-main.535e2f4c.js"
+  },
+  {
+    "revision": "22b4c4a72c8c750c24b05cd20df0f610",
+    "url": "/COVID-19-Summary/static/media/caret.22b4c4a7.svg"
+  },
+  {
+    "revision": "0ed912cb56f853c5784ca940d5ed921e",
+    "url": "/COVID-19-Summary/static/media/chevron.0ed912cb.svg"
+  }
+]);
